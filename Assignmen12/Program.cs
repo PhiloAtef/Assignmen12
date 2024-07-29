ee27b82fc010889116623a6ea4b432082b0d31db
Philo <philoatefdev@gmail.com>
2024-07-29 21:39:09 +0300

    Add project files.

---
+++ Assignmen12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignmen12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
